aec82758fc5d7275d0ed86a24f03a40665cb0f49
Datorium <hello@datorium.lv>
2021-02-25 20:31:43 +0200

    Digital clock ready

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DigitalClockWinForms
 {
-    partial class Form1
+    partial class Clock
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,56 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.Display = new System.Windows.Forms.Label();
+            this.ClockTimer = new System.Windows.Forms.Timer(this.components);
+            this.ButtonStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Display
+            // 
+            this.Display.AutoSize = true;
+            this.Display.Font = new System.Drawing.Font("Agency FB", 80F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Display.Location = new System.Drawing.Point(39, 39);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(539, 158);
+            this.Display.TabIndex = 0;
+            this.Display.Text = "00:00:00:0";
+            // 
+            // ClockTimer
+            // 
+            this.ClockTimer.Tick += new System.EventHandler(this.ClockTimer_Tick);
+            // 
+            // ButtonStop
+            // 
+            this.ButtonStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonStop.Font = new System.Drawing.Font("Agency FB", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonStop.Location = new System.Drawing.Point(191, 209);
+            this.ButtonStop.Name = "ButtonStop";
+            this.ButtonStop.Size = new System.Drawing.Size(215, 71);
+            this.ButtonStop.TabIndex = 1;
+            this.ButtonStop.Text = "Stop";
+            this.ButtonStop.UseVisualStyleBackColor = true;
+            this.ButtonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // Clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(603, 332);
+            this.Controls.Add(this.ButtonStop);
+            this.Controls.Add(this.Display);
+            this.Name = "Clock";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Display;
+        private System.Windows.Forms.Timer ClockTimer;
+        private System.Windows.Forms.Button ButtonStop;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,63 @@ using System.Windows.Forms;
 
 namespace DigitalClockWinForms
 {
-    public partial class Form1 : Form
+    public partial class Clock : Form
     {
-        public Form1()
+        private int dsec = 0;
+        private int sec = 0;
+        private int min = 0;
+        private int hou = 0;
+
+        public Clock()
         {
             InitializeComponent();
+            this.BackColor = Color.Black;
+            Display.ForeColor = Color.White;
+            ButtonStop.ForeColor = Color.White;
+            ClockTimer.Start();
+        }
+
+
+        private void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            string dsecText;
+            string secText;
+            string minText;
+            string houText;
+            
+            dsec += 1;
+            if(dsec == 10)
+            {
+                sec += 1;
+                dsec = 0;                
+            }
+
+            if(sec == 60)
+            {
+                min += 1;
+                sec = 0;
+            }
+
+            if(min == 60)
+            {
+                hou += 1;
+                min = 0;
+            }
+
+            dsecText = dsec.ToString();
+            secText = sec.ToString("00");
+            minText = min.ToString("00");
+            houText = hou.ToString("00");
+            Display.Text = 
+                houText + ":" + 
+                minText + ":" +
+                secText + ":" +
+                dsecText;
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            ClockTimer.Stop();
         }
     }
 }
